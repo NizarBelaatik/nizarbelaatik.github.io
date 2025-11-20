6eef0d26227452edefc19cb0eda23da191baea00
NizarBelaatik <belaatiknizar@gmail.com>
2025-11-20 18:07:54 +0100

    testing deploy 3

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -95,32 +95,16 @@ const Header = () => {
                   {item.name}
                 </a>
               ) : (
-                
-                
-
-                item.nav_name === 'projects' ? (
-                  <Link
-                    key={item.name}
-                    to="/projects" // for SPA routes
-                    className={`text-lg font-medium transition-colors hover:text-accent-blue ${
-                      location.pathname === item.href ? 'text-accent-blue' : 'text-white'
-                    }`}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    {item.name}
-                  </Link>
-                ) : (
-                  <Link
-                    key={item.name}
-                    to={item.href} // for SPA routes
-                    className={`text-lg font-medium transition-colors hover:text-accent-blue ${
-                      location.pathname === item.href ? 'text-accent-blue' : 'text-white'
-                    }`}
-                    onClick={() => setIsMenuOpen(false)}
-                  >
-                    {item.name}
-                  </Link>
-                )
+                <Link
+                  key={item.name}
+                  to={item.href} // for SPA routes
+                  className={`text-lg font-medium transition-colors hover:text-accent-blue ${
+                    location.pathname === item.href ? 'text-accent-blue' : 'text-white'
+                  }`}
+                  onClick={() => setIsMenuOpen(false)}
+                >
+                  {item.name}
+                </Link>
               )
             )}
           </nav>
