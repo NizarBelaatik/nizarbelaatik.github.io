f04fe154e6be1a07e114cc9e1b6c8347da11d333
NizarBelaatik <belaatiknizar@gmail.com>
2025-11-20 17:03:43 +0100

    testing deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter as Router, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './hooks/useTheme.jsx'
 import Header from './components/layout/Header.jsx'
 import Footer from './components/layout/Footer.jsx'
